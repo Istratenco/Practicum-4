594b06e9f4c8e5fd1c31afb305be9ca2832f022f
Istratenco <162767086+Istratenco@users.noreply.github.com>
2024-12-13 23:02:02 +0300

    Add files via upload

---
+++ practicum 4/практикум 4/Program.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+
+            //Console.Write("Число ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    Console.WriteLine(++a);
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{a - 2}");
+            //}
+
+            //Задача 2
+
+            //Console.Write("Число ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    Console.WriteLine(++a);
+            //}
+            //Console.WriteLine(a);
+
+
+            //Задача 3
+
+            // Console.Write("Число ");
+            // int a = int.Parse(Console.ReadLine());
+            // if (a > 0)
+            // {
+            //     Console.WriteLine(++a);
+            // }
+            //if (a < 0)
+            // {
+            //     Console.WriteLine($"{a - 2}");
+            // }
+            //if (a==0)
+            // {
+            //     Console.WriteLine($"{a + 10}");
+            // }
+
+            //Задача 4
+
+            //Console.Write("Число ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a/2)
+            //{
+            //    Console.WriteLine("Четное");
+            //}    
+            //else
+            //{
+            //    Console.WriteLine("Нечетное");
+            //}
+
+
+            //int x = 3;
+            //int y = 2;
+            //int z = x < y ? (x + y) : (x - y);
+            //Console.WriteLine(z);   // 1
+
+
+            //Console.Write("x = ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    Console.WriteLine(++x);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(--x);
+            //}
+            //int y = x > 0 ? (x + 1) : (x - 1);
+            //Console.WriteLine(y);
+
+            //Console.Write("a = ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("b = ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("c = ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int min = a > b ? a : b;
+            //int max = min > c ? min : c;
+            //Console.WriteLine(max);
+
+            //Задача 5
+            //Console.Write("a = ");
+            //int a = int.Parse(Console.ReadLine());
+            //if (a > 0)
+            //{
+            //    Console.WriteLine("Положительное");
+            //}
+            //if (a < 0)
+            //{
+            //    Console.WriteLine("Отрицательное");
+            //}
+            //if (a == 0)
+            //{
+            //    Console.WriteLine("Нулевое");
+            //}
+
+            //Задача 6
+
+            //Console.Write("Введите цену ноутбука");
+            //int price = Convert.ToInt32(Console.ReadLine());
+            //if (price >20000 && price <30000)
+            //{
+            //    Console.Write("Цена входит в диапозон");
+            //}
+            //else
+            //{
+            //    Console.Write("Цена не входит в диапазон");
+            //}
+
+            //Задача 7
+
+            //Console.Write("Введите год ");
+            //int Year = Convert.ToInt32(Console.ReadLine());
+            //if ((Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0))
+            //{ 
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+            //Console.ReadKey();
+        }
+    }
+}
